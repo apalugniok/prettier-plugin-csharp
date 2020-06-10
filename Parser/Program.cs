3067a37d81a0dd32169962a9b79ec503def244aa
apalugniok <andypalugniok@gmail.com>
2020-06-10 17:10:56 +0100

    Initial commit

---
+++ Parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrettierCSharpPlugin.Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
